91c8c81d2734ab22e48bc84b2be66af27e3f42dd
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2021-06-07 18:19:25 +0200

    Create newton.m

---
+++ newton.m
@@ -0,0 +1,23 @@
+function [ output_args ] = newton(f, x0, tol, itr )
+%NEWTONH-RAPHSON: Find the root of the given function using Newton-Raphson method
+%   Input:
+%           f: The input function to calculate its root
+%           x0: initial input
+%           tol: Error tolerance 
+%           itr: Iterations limit
+%   This function takes the function, initial input, error tolerance and
+%   iterations limit to compute the root using Newton-Raphson method.
+
+syms x;
+der=diff(f);
+
+for i=2:itr
+    f0=subs(f,'x',x0);
+    f0_der=subs(der,'x',x0);
+    x1=x0-(double(f0)/double(f0_der));
+    if abs(x1-x0)<tol,break,end
+    x0=x1;
+end 
+output_args = num2str(x1);
+end
+
