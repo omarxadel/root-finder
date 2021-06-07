8818bb8553353031489d9b8d7b3f69cf4dadc1bc
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-06-07 18:42:27 +0200

    Update GUI.m
    Rearrange functions calls
    Handle GUI input cases

--- GUI.m
+++ GUI.m
@@ -86,142 +86,6 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-
-
-% --- Executes during object creation, after setting all properties.
-function interval_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to interval (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function interval_Callback(hObject, eventdata, handles)
-% hObject    handle to interval (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of interval as text
-%        str2double(get(hObject,'String')) returns contents of interval as a double
-
-int=get(handles.interval,'String');
-if (get(handles.bisection, 'Value') == 1) || (get(handles.falsepos, 'Value') == 1)
-    int=strsplit(int);
-    if length(int) < 2
-        set(hObject, 'String', 0);
-        errordlg('Interval must be valid','Error');
-    end
-elseif (get(handles.newt, 'Value') == 1)
-    if isnan(str2double(int))
-        set(hObject, 'String', 0);
-        errordlg('Xo must be valid','Error');
-    end
-end
-
-guidata(hObject,handles)
-
-% --- Executes on button press in calculate.
-function calculate_Callback(hObject, eventdata, handles)
-% hObject    handle to calculate (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-syms x;
-a=get(handles.func,'String');
-f=inline(a);
-int=get(handles.interval,'String');
-
-tol=str2double(get(handles.tol,'String'));
-itr=str2double(get(handles.itr,'String'));
-
- if(get(handles.bisection,'Value') == 1)
-    int=strsplit(int);
-    xl=str2double(int(1));
-    xu=str2double(int(2));
-    if f(xu)*f(xl)<0
-        ans = bisection(f, xl, xu, tol, itr);
-        set(handles.ans, 'String', ans);
-    else
-        errordlg('f(xu)*f(xl) must be <0','Error');
-    end
- elseif(get(handles.falsepos,'Value') == 1)
-    int=strsplit(int);
-    xl=str2double(int(1));
-    xu=str2double(int(2));
-    if f(xu)*f(xl)<0
-        ans = false_pos(f, xl, xu, tol, itr);
-        set(handles.ans, 'String', ans);
-    else
-        errordlg('f(xu)*f(xl) must be <0','Error');
-    end
- elseif(get(handles.newt,'Value') == 1)
-     f1=inline2sym(f);
-     xo = str2double(int);
-     ans = newton(f1, xo, tol, itr);
-     set(handles.ans, 'String', ans);
- end
-     
-% --- Executes on button press in reset.
-function reset_Callback(hObject, eventdata, handles)
-% hObject    handle to reset (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-initialize_gui(gcbf, handles, true);
-
-% --- Executes when selected object changed in unitgroup.
-function unitgroup_SelectionChangedFcn(hObject, eventdata, handles)
-% hObject    handle to the selected object in unitgroup 
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-if (hObject == handles.bisection) || (hObject == handles.falsepos)
-    set(handles.interval, 'enable', 'on');  
-    set(handles.interval_name, 'String', 'Interval');  
-elseif (hObject == handles.newt)
-    set(handles.interval, 'enable', 'on');  
-    set(handles.interval_name, 'String', 'Xo');  
-else
-    set(handles.interval, 'enable', 'off');
-end
-
-% --------------------------------------------------------------------
-function initialize_gui(fig_handle, handles, isreset)
-% If the metricdata field is present and the reset flag is false, it means
-% we are we are just re-initializing a GUI by calling it from the cmd line
-% while it is up. So, bail out as we dont want to reset the data.
-if isfield(handles, 'metricdata') && ~isreset
-    return;
-end
-
-set(handles.func, 'String', 0);
-set(handles.interval,  'String', 0);
-set(handles.itr, 'String', 0);
-set(handles.tol,  'String', 0);
-set(handles.ans, 'String', 0);
-
-set(handles.unitgroup, 'SelectedObject', handles.bisection);
-
-% Update handles structure
-guidata(handles.figure1, handles);
-
-
-% --- Executes on key press with focus on bisection and none of its controls.
-function bisection_KeyPressFcn(hObject, eventdata, handles)
-% hObject    handle to bisection (see GCBO)
-% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.UICONTROL)
-%	Key: name of the key that was pressed, in lower case
-%	Character: character interpretation of the key(s) that was pressed
-%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
-% handles    structure with handles and user data (see GUIDATA)
-
-
-
 function func_Callback(hObject, eventdata, handles)
 % hObject    handle to func (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -247,8 +111,6 @@ end
 % end
 
 guidata(hObject,handles)
-
-
 
 
 function itr_Callback(hObject, eventdata, handles)
@@ -306,3 +168,157 @@ function tol_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+
+% --- Executes during object creation, after setting all properties.
+function interval_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to interval (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function interval_Callback(hObject, eventdata, handles)
+% hObject    handle to interval (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of interval as text
+%        str2double(get(hObject,'String')) returns contents of interval as a double
+
+int=get(handles.interval,'String');
+if (get(handles.bisection, 'Value') == 1) || (get(handles.falsepos, 'Value') == 1)
+    int=strsplit(int);
+    if length(int) < 2
+        set(hObject, 'String', 0);
+        errordlg('Interval must be valid','Error');
+    end
+elseif (get(handles.newt, 'Value') == 1)
+    if isnan(str2double(int))
+        set(hObject, 'String', 0);
+        errordlg('Xo must be valid','Error');
+    end
+end
+
+guidata(hObject,handles)
+
+% --- Executes on button press in calculate.
+function calculate_Callback(hObject, eventdata, handles)
+% hObject    handle to calculate (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Function
+a=get(handles.func,'String');
+f=inline(a);
+
+% Interval
+int=get(handles.interval,'String');
+
+% Tolerance
+tol=str2double(get(handles.tol,'String'));
+
+% Iteration
+itr=str2double(get(handles.itr,'String'));
+
+% Bisection
+ if(get(handles.bisection,'Value') == 1)
+    int=strsplit(int);
+    
+    if length(int) < 2
+        set(handles.interval, 'String', 0);
+        errordlg('Interval must be valid','Error');
+    end
+    
+    xl=str2double(int(1));
+    xu=str2double(int(2));
+    
+    if f(xu)*f(xl)<0
+        ans = bisection(f, xl, xu, tol, itr);
+        set(handles.ans, 'String', ans);
+    else
+        errordlg('f(xu)*f(xl) must be <0','Error');
+    end
+% False-Position
+ elseif(get(handles.falsepos,'Value') == 1)
+    int=strsplit(int);
+    
+    if length(int) < 2
+        set(handles.interval, 'String', 0);
+        errordlg('Interval must be valid','Error');
+    end
+    
+    xl=str2double(int(1));
+    xu=str2double(int(2));
+    
+    if f(xu)*f(xl)<0
+        ans = false_pos(f, xl, xu, tol, itr);
+        set(handles.ans, 'String', ans);
+    else
+        errordlg('f(xu)*f(xl) must be <0','Error');
+    end
+ % Newton-Raphson
+ elseif(get(handles.newt,'Value') == 1)
+    xo = str2double(int);
+    if isnan(xo)
+        set(handles.interval, 'String', 0);
+        errordlg('Xo must be valid','Error');
+    end
+     
+     syms x;
+     f1=inline2sym(f);
+     
+
+     
+     ans = newton(f1, xo, tol, itr);
+     set(handles.ans, 'String', ans);
+ end
+     
+% --- Executes on button press in reset.
+function reset_Callback(hObject, eventdata, handles)
+% hObject    handle to reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+initialize_gui(gcbf, handles, true);
+
+% --- Executes when selected object changed in unitgroup.
+function unitgroup_SelectionChangedFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in unitgroup 
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.interval,  'String', 0);
+if (hObject == handles.bisection) || (hObject == handles.falsepos)
+    set(handles.interval, 'enable', 'on');  
+    set(handles.interval_name, 'String', 'Interval');  
+elseif (hObject == handles.newt)
+    set(handles.interval, 'enable', 'on');  
+    set(handles.interval_name, 'String', 'Xo');  
+else
+    set(handles.interval, 'enable', 'off');
+end
+
+% --------------------------------------------------------------------
+function initialize_gui(fig_handle, handles, isreset)
+% If the metricdata field is present and the reset flag is false, it means
+% we are we are just re-initializing a GUI by calling it from the cmd line
+% while it is up. So, bail out as we dont want to reset the data.
+
+set(handles.func, 'String', 0);
+set(handles.interval,  'String', 0);
+set(handles.itr, 'String', 50);
+set(handles.tol,  'String', 0.00001);
+set(handles.ans, 'String', 0);
+
+set(handles.unitgroup, 'SelectedObject', handles.bisection);
+set(handles.interval_name, 'String', 'Interval');
+
+% Update handles structure
+guidata(handles.figure1, handles);
